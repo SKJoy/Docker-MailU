01977aad05d9cbd008fd4a1c28b43f89ae53f462
Broken Arrow <SKJoy2001@GMail.Com>
2025-11-23 02:23:02 +0600

    Self signed SSL for wildcard domain

--- Sample-SSL-Generate-Self-signed.sh
+++ Sample-SSL-Generate-Self-signed.sh
@@ -9,19 +9,21 @@ THIS_SCRIPT_PATH="$(dirname "$0")/"
 cd ${THIS_SCRIPT_PATH};
 
 # User customization
-HOSTNAME="email.domain.tld"
+DOMAIN="domain.tld"
 
 # EVERYTHING BELOW IS AUTOMATED; DO NOT CHANGE
+source .env
 MAILU_PATH="${THIS_SCRIPT_PATH}Volume/MailU/SSL/"
 
 mkdir ${MAILU_PATH} -p
-openssl req -x509 -newkey rsa:4096 -keyout ${MAILU_PATH}key.pem -out ${MAILU_PATH}cert.pem -sha256 -days 3650 -nodes -subj "/CN=${HOSTNAME}"
+openssl req -x509 -newkey rsa:4096 -keyout ${MAILU_PATH}key.pem -out ${MAILU_PATH}cert.pem -sha256 -days 3650 -nodes -subj "*.${DOMAIN}/CN=${DOMAIN}"
 docker compose restart
-echo "${CURRENT_DATE} ${CURRENT_TIME} Self signed SSL generated for ${HOSTNAME}" >> ${THIS_SCRIPT_PATH}Action.log
+echo "${CURRENT_DATE} ${CURRENT_TIME} Self signed SSL generated for ${DOMAIN}" >> ${THIS_SCRIPT_PATH}Action.log
 
 cat <<CONTENT
 
 ⭐ Self signed SSL certificate generated
+   ❓ Domain        : '${DOMAIN}'
    ❓ Path        : '${MAILU_PATH}'
    ⏰ Time        : ${CURRENT_DATE} ${CURRENT_TIME}
 
