f71cd70e3bcc73009e96da97c203858bbe246539
Broken Arrow <SKJoy2001@GMail.Com>
2025-11-23 02:33:27 +0600

    Self signed wildcard SSL certificate

--- Sample-SSL-Generate-Self-signed.sh
+++ Sample-SSL-Generate-Self-signed.sh
@@ -16,7 +16,7 @@ source .env
 MAILU_PATH="${THIS_SCRIPT_PATH}Volume/MailU/SSL/"
 
 mkdir ${MAILU_PATH} -p
-openssl req -x509 -newkey rsa:4096 -keyout ${MAILU_PATH}key.pem -out ${MAILU_PATH}cert.pem -sha256 -days 3650 -nodes -subj "*.${DOMAIN}/CN=${DOMAIN}"
+openssl req -x509 -newkey rsa:4096 -keyout ${MAILU_PATH}key.pem -out ${MAILU_PATH}cert.pem -sha256 -days 3650 -nodes -subj "/CN=${DOMAIN}" -addext "subjectAltName = DNS:*.${DOMAIN}"
 docker compose restart
 echo "${CURRENT_DATE} ${CURRENT_TIME} Self signed SSL generated for ${DOMAIN}" >> ${THIS_SCRIPT_PATH}Action.log
 
